6e2557d46fa4ac4843e07d344ccbbab7658e6094
Hamza Simsek <hamzas4011@hotmail.com>
2025-07-03 23:22:58 +0200

    fixed the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,9 @@
 const Footer = () => {
-    return (
-      <footer className="bg-[#1e293b] text-center text-white py-4 text-sm">
-        &copy; {new Date().getFullYear()} <strong>WorldView</strong>. All rights reserved.
-      </footer>
-    );
-  };
-  
-  export default Footer;
-  
+  return (
+    <footer className="bg-[#1e293b] text-white text-center py-4 text-sm w-full">
+      &copy; {new Date().getFullYear()} <strong>WorldView</strong>. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
