7a59cefb079abb448b2a4f31d2436de41a49b445
Hamza Simsek <hamzas4011@hotmail.com>
2025-07-04 00:55:48 +0200

    testing size for mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,24 @@
-import './globals.css';
-import { ReactNode } from 'react';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import './globals.css'
+import { ReactNode } from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 export const metadata = {
   title: 'World View App',
   description: 'Explore global news, cultures, and more',
-};
+}
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="flex flex-col min-h-screen">
         <Navbar />
-
-        <main className="flex-grow min-h-[500px]">
+        
+        <main className="flex-grow">
           {children}
         </main>
-
-        <Footer />
+         <Footer />
       </body>
     </html>
-  );
+  )
 }
